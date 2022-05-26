1952d307af9b680cb3d170022e6ada45ca0d532c
Sergio Dobri <sergiodobri@gmail.com>
2022-05-26 15:55:39 -0300

    add DarkMode to SettingForm

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import PageHeader from 'src/components/page/PageHeader.vue';
 import PageFooter from 'src/components/page/PageFooter.vue';
 
-import LeftSidebar from 'src/components/LeftSidebar.vue';
-import DarkModeToggle from 'src/components/DarkModeToggle.vue';
+import LeftSidebar from 'src/components/menu/LeftSidebar.vue';
 
-export { PageHeader, PageFooter, LeftSidebar, DarkModeToggle };
+export { PageHeader, PageFooter, LeftSidebar };
--- src/pages/settings/SettingsForm.vue
+++ src/pages/settings/SettingsForm.vue
@@ -1,9 +1,24 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch, onMounted } from 'vue';
+import { useQuasar } from 'quasar';
 import { PageHeader } from 'src/components';
 
 const form = ref({
+  dark_mode: false,
   stock_is_automatic: false
+});
+const $q = useQuasar();
+
+watch(form.value.dark_mode, () => {
+  $q.dark.set(form.value.dark_mode);
+  $q.localStorage.set('darkMode', form.value.dark_mode);
+});
+
+onMounted(() => {
+  const darkModeIsActive = $q.localStorage.getItem('darkMode');
+  if (darkModeIsActive) {
+    form.value.dark_mode = true;
+  }
 });
 </script>
 
@@ -13,10 +28,20 @@ const form = ref({
       <template #title>Endless</template>
     </page-header>
     <q-list class="rounded-borders">
-      <q-item
-        dense
-        tag="label"
-      >
+      <q-item tag="label">
+        <q-item-section>
+          <q-item-label>Utilizar modo escuro</q-item-label>
+        </q-item-section>
+        <q-item-section avatar>
+          <q-toggle
+            v-model="form.dark_mode"
+            checked-icon="sym_r_dark_mode"
+            unchecked-icon="sym_r_light_mode"
+            val="picture"
+          />
+        </q-item-section>
+      </q-item>
+      <q-item tag="label">
         <q-item-section>
           <q-item-label>Utilizar estoque automático</q-item-label>
         </q-item-section>
