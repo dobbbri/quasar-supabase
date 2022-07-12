3e5cda39da782b8cd2e8bd6c7e186f89acf7634c
Sergio Dobri <sergiodobri@gmail.com>
2022-07-12 17:37:49 -0300

    add bd image

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -17,10 +17,10 @@ import { LeftSidebar } from 'src/components';
 .app {
   width: 100%;
   height: 100%;
-  background: transparent;
-  /* background-image: url(../assets/hero-bg.svg); */
-  /* background-position: top center; */
-  /* background-repeat: no-repeat; */
+  /* background: transparent; */
+  background-image: url(../assets/hero-bg.svg);
+  background-position: top center;
+  background-repeat: no-repeat;
   &-layout {
     margin: 0 auto;
     z-index: 4000;
