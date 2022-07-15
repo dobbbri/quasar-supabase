8aba13968081ee8562a31f8223ec38839d25bba2
Sergio Dobri <sergiodobri@gmail.com>
2022-07-15 01:49:46 -0300

    fixes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,7 +7,7 @@ import { LeftSidebar } from 'src/components';
     <q-layout view="lHh Lpr lFf" class="app-layout shadow-5" container>
       <left-sidebar />
       <q-page-container class="bg-white">
-        <router-view :key="$route.fullPath" style="overflow: hidden" />
+        <router-view :key="$route.fullPath" />
       </q-page-container>
     </q-layout>
   </div>
